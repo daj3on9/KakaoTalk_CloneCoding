d965d62810b121490cb49f4ded69ce08d9abca1f
daj3on9 <dajung5623@naver.com>
2025-04-03 16:42:14 +0900

    ✨ Feat : Add Register UI

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Login from "./page/auth/login";
+import Register from "./page/auth/Register";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
---
+++ src/page/auth/Register.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import "../../css/auth/Register.css";
+
+function Register() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    alert("제출 성공!", data);
+  };
+
+  // 비밀번호와 비밀번호 확인 비교용
+  const password = watch("password");
+
+  return (
+    <div className="register-container">
+      <h1> kakao </h1>
+      <div className="register-box">
+        <div className="register-content">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="register-input-box">
+              <input
+                type="text"
+                placeholder="이메일"
+                {...register("email", { required: "이메일을 입력해주세요." })}
+              />
+              <input
+                type="password"
+                placeholder="비밀번호"
+                {...register("password", {
+                  required: "비밀번호를 입력해주세요.",
+                })}
+              />
+              <input
+                type="password"
+                placeholder="비밀번호 확인"
+                {...register("passwordCheck", {
+                  required: "비밀번호 확인이 필요합니다.",
+                  validate: (value) =>
+                    value === password || "비밀번호가 일치하지 않습니다.",
+                })}
+              />
+              <input
+                type="text"
+                placeholder="닉네임"
+                {...register("nickname", {
+                  required: "닉네임을 입력해주세요.",
+                })}
+              />
+            </div>
+
+            <div className="button-box">
+              <p>
+                {errors.email?.message ||
+                  errors.password?.message ||
+                  errors.passwordCheck?.message ||
+                  errors.nickname?.message}
+              </p>
+              <button type="submit">회원가입</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
