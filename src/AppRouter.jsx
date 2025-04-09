505394dcb710c980a420382ff843cd31f61dfcf0
daj3on9 <dajung5623@naver.com>
2025-04-09 17:56:08 +0900

    ✨ Feat : Add ChatRooms Page

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Login from "./page/auth/Login";
+import Login from "./page/auth/login";
 import Register from "./page/auth/Register";
+import ChatRooms from "./page/ChatRooms";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/chatRooms" element={<ChatRooms />} />
     </Routes>
   );
 }
---
+++ src/page/ChatRooms.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChatRooms() {
+  return (
+    <div>
+      <p> 채팅방 리스트 화면 </p>
+    </div>
+  );
+}
+
+export default ChatRooms;
