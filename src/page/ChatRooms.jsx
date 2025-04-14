ae04a939ced766be736d5a61a6fc83cebd0240ae
daj3on9 <dajung5623@naver.com>
2025-04-14 17:40:13 +0900

    ✨ Feat: Add API for fetching current user information & user chatLists

--- src/page/ChatRooms.jsx
+++ src/page/ChatRooms.jsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/ChatRooms.css";
 import { useName } from "../store/useStore";
+import { authGetAPI } from "../api/customAPI";
+import ChatProfileCard from "../component/ChatProfileCard";
 
 function ChatRooms() {
   const userName = useName();
+  const [myInfo, setMyInfo] = useState({});
+  const [chatList, setChatList] = useState([]);
 
-  /*
+  // 내 정보 불러오기
+  const getMyInfo = async () => {
+    const responseData = await authGetAPI("/users/me");
+    if (responseData) {
+      setMyInfo(responseData);
+    } else {
+      console.error("내 정보 불러오기 실패");
+    }
+  };
+
+  // 채팅방 목록 불러오기
+  const getChatLists = async () => {
+    const responseData = await authGetAPI("/users/me/chatrooms");
+    if (responseData) {
+      setChatList(responseData);
+    } else {
+      console.error("채팅방 목록 불러오기 에러");
+    }
+  };
+
   React.useEffect(() => {
-    console.log(userName);
+    // console.log(userName);
+    getMyInfo();
+    getChatLists();
   }, [userName]);
+
+  // 확인
+  /*
+  const check = () => {
+    console.log("채팅방 목록 : ", chatList);
+    console.log(chatList.map((room) => console.log(room.id)));
+  };
+  
+  React.useEffect(() => {
+    check();
+  }, [chatList]);
   */
 
   return (
@@ -18,6 +54,10 @@ function ChatRooms() {
         <p>
           <span>{userName}</span> 님의 채팅
         </p>
+        <div>
+          <ChatProfileCard data={myInfo} state="me" />
+        </div>
+        <div className="under-line"></div>
       </div>
     </>
   );
